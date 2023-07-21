ad5171a1be1cd530145f836f6a4e96a42cf021ae
Rodrigou0 <127239242+Rodrigou0@users.noreply.github.com>
2023-07-21 17:56:07 +0000

    start wars

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -38,12 +38,104 @@ def sitemap():
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    users = User.query.all()
+    results = []
+    for user in users:
+        user_data = {
+            'id': user.id,
+            'email': user.email,
+        }
+        results.append(user_data)
+    return jsonify(results)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people', methods=['GET'])
+def get_people():
+    people = People.query.all()
+    results = []
+    for person in people:
+        person_data = {
+            'id': person.id,
+            'name': person.name,
+            'gender': person.gender,
+            'lastname': person.lastname
+        }
+        results.append(person_data)
+    return jsonify(results)
 
-    return jsonify(response_body), 200
+@app.route('/people', methods=['POST'])
+def add_person():
+    data = request.get_json()
+    new_person = People(name=data['name'], gender=data['gender'], lastname=data['lastname'])
+    db.session.add(new_person)
+    db.session.commit()
+    return jsonify({'message': 'Person created successfully'}),200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_person(id):
+    person = People.query.get(id)
+    return jsonify({'data':person.serialize()}),200
+
+
+@app.route('/planets', methods=['POST'])
+def add_planet():
+    data = request.get_json()
+    new_planet = Planets(name=data['name'], size=data['size'], population=data['population'])
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify({'message': 'Planet created successfully'}),200
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_planet(id):
+    planet = Planets.query.get(id)
+    return jsonify({'data':planet.serialize()}),200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planets.query.all()
+    results = []
+    for planet in planet:
+        planet_data = {
+            'id': planet.id,
+            'name': planet.name,
+            'size': planet.size,
+            'population': planet.population
+        }
+        results.append(planet_data)
+    return jsonify(results)
+
+@app.route('/favorites', methods=['GET'])
+def get_favorites():
+    favorites = favorites.query.all()
+    results = []
+    for favorite in favorites:
+        favorite_data = {
+            'id': favorite.id,
+            'people': favorite.people_id,
+            'planets': favorite.planets_id,
+            'users': favorite.user_id
+        }
+        results.append(favorite_data)
+    return jsonify(results)
+
+@app.route('/favorites', methods=['POST'])
+def add_planets_favorites():
+    data = request.get_json()
+    new_favorites = Favorites(people_id=data['people_id'], planets_id=data['planets_id'], user_id=data['user_id'])
+    db.session.add(new_favorites)
+    db.session.commit()
+    return jsonify({'message': 'added to favorites'}),200
+
+@app.route('/favorites/<int:favorite_id>', methods=['DELETE'])
+def delete_favorite(favorite_id):
+    favorite_to_delete = Favorites.query.filter_by(id=favorite_id).first()
+
+    if favorite_to_delete:
+        db.session.delete(favorite_to_delete)
+        db.session.commit()
+        return jsonify({'message': 'Favorite deleted successfully'}), 200
+    else:
+        return jsonify({'error': 'Favorite not found'}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+            return '<User %r>' % self.id
 
     def serialize(self):
         return {
@@ -17,3 +17,59 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    gender = db.Column(db.String(120), unique=False, nullable=False)
+    lastname = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<people %r>' %self.id
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "lastname": self.lastname
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    population = db.Column(db.String(120), unique=False, nullable=False)
+    size = db.Column(db.String(120), unique=False, nullable=False)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<planets %r>' %self.id
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name":self.name,
+            "size": self.size,
+            "population": self.population,
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    planets_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    people = db.relationship("People")
+    user = db.relationship("User")
+    planets = db.relationship("Planets")
+
+    def __repr__(self):
+        return '<favorites %r>' %self.id
+    
+    def serialize(self):
+        return{
+            "id": self.id,
+            "people":self.people_id,
+            "planets": self.planets_id,
+            "user": self.user_id,
+        }
+
+
+    
